039c963d7f45661d5100c58929e00ce723d9b5c9
Dustin Elery <delery7@yahoo.com>
2020-12-28 16:42:48 -0500

    started PyBank but cannot get a column to sum

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,12 +10,21 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    # print(csvreader)
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
+    #print(f"CSV Header: {csv_header}")
+row_count=0
     # Read each row of data after the header
-    for row in csvreader:
-        print(row)
+for row in csvpath:
+    row_count+=1
+print(f'The total row count is: {row_count}')
+
+
+sum_PandL=0
+for rows in csvpath:
+   sum_PandL += int(rows[1])
+
+
+#print("sum_PandL")
