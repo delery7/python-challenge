2cb568b66438fd6a1f0e484853802226da976959
Dustin Elery <delery7@yahoo.com>
2020-12-28 15:39:17 -0500

    getting the csv to pull into my py file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,21 @@
-print("adding a main file to PyBank")
+# print("adding a main file to PyBank")
+import os
+import csv
+
+# csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = 'c:/Users/deler/Desktop/python-challenge/PyBank/Resources/budget_data.csv'
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
