b34f337ec06fdd06c2ac49b6126203d1b4a2456f
Dustin Elery <delery7@yahoo.com>
2021-01-04 17:22:07 -0500

    Got the Bank py file working except for dynamic file paths

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,53 +11,54 @@ df=pd.read_csv(csvpath)
 difference=0.0
 df['Change']=df['Profit/Losses'] - df['Profit/Losses'].shift(1)
 average=df['Change'].mean()
-#max=df[df['Profit/Losses']==df['Profit/Losses'].max()]
-#max=df.groupby(['Date','Profit/Losses'], as_index=False)['Profit/Losses'].max()
-maxvalue=df['Profit/Losses'].max()
-maxindex=df['Profit/Losses'].idxmax()
+maxvalue=df['Change'].max()
+maxindex=df['Change'].idxmax()
 maxmonth=df.loc[maxindex,'Date']
 
-minvalue=df['Profit/Losses'].min()
-minindex=df['Profit/Losses'].idxmin()
+minvalue=df['Change'].min()
+minindex=df['Change'].idxmin()
 minmonth=df.loc[minindex,'Date']
 #print(df)
 with open(csvpath) as csvfile:
 
-    # CSV reader specifies delimiter and variable that holds contents
+    # CSV reader used to declare delimiter and variable that holds contents into a variable I can reference
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    # print(csvreader)
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     # print(f"CSV Header: {csv_header}")
+
     row_count=0
-    averages=0.0
     Sum=0.0
     PandL=0.0
-    change=0.0
-    #     # Read each row of data after the header
+
+    # Tried to start this without pandas, so the count and P&L Sum is done without a dataframe
     for row in csvreader:
-    #print(row[0] + " and " + row[1])
-        #for column in row.split(','):
+
         PandL = float(row[1])
         Sum += PandL
         row_count+=1
        # averages=Sum/row_count
-    # for i, rows in csvreader:
-    #     for j,rows in csvreader:
-    #        change= rows[j]-rows[i-1]
-    #     print(rows[j])
 
-    
-    print(round(average,2))
-    print(maxvalue)
-    print(maxindex)
-    print(maxmonth)
-    print(f'{minvalue} {minmonth}')
-    print(f'The total row count is: {row_count}')
-    print(f'The total sum of columns is: {Sum}')
-    print(f'Change is {change}')
+
+    print('Financial Analysis')
+    print('-------------------')
+    print(f'Total Months: {row_count}')
+    print(f'Total P&L: ${Sum}')
+    print(f'Average Change: ${round(average,2)}')
+    print(f'Greatest Increase in Profits: {maxmonth} ${maxvalue}')
+    print(f'Greatest Decrease in Profits: {minmonth} ${minvalue}')
+
+    outputPath=r"C:\Users\deler\Desktop\python-challenge\PyBank\Analysis\Output.txt"
+    with open(outputPath, 'w', newline='') as txtfile:
+        txtfile.write('Financial Analysis\n')
+        txtfile.write('-------------------\n')
+        txtfile.write(f'Total Months: {row_count}\n')
+        txtfile.write(f'Total P&L: ${Sum}\n')
+        txtfile.write(f'Average Change: ${round(average,2)}\n')
+        txtfile.write(f'Greatest Increase in Profits: {maxmonth} ${maxvalue}\n')
+        txtfile.write(f'Greatest Decrease in Profits: {minmonth} ${minvalue}\n')
+        txtfile.close()
 
 
 
