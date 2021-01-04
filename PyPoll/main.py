ecfcd0f341e200d0ee1336a3d76df55fddde09c4
Dustin Elery <delery7@yahoo.com>
2021-01-04 18:55:53 -0500

    Got the math working and print statement ready but need to clean formatting

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,52 +3,37 @@ import os
 import csv
 import pandas as pd
 
+
 #csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 csvpath = r"C:\Users\deler\Desktop\python-challenge\PyPoll\Resources\election_data.csv"
 #print(csvpath)
 df=pd.read_csv(csvpath)
-print(df)
+#print(df)
 
 vote_count=df['Voter ID'].nunique()
-print(vote_count)
+# print(vote_count)
+cc=df.groupby(['Candidate']).nunique()
+ccpercent=(cc/vote_count)*100
+candidatesvotes=cc.iloc[:,0]
+candidatepercent=ccpercent.iloc[:,0]
 
+maxname=cc.max(axis=1)
+print(maxname)
+#print(cc)
+# print(ccpercent)
+# print(candidatesvotes)
+# print(candidatepercent)
 
-# difference=0.0
-# df['Change']=df['Profit/Losses'] - df['Profit/Losses'].shift(1)
-# average=df['Change'].mean()
-# maxvalue=df['Change'].max()
-# maxindex=df['Change'].idxmax()
-# maxmonth=df.loc[maxindex,'Date']
+print("     ")
+print('Election Results')
+print('-------------------')
+print(f'Total Votes: {vote_count}')
+print('-------------------')
+print(f'{candidatepercent} {candidatesvotes}')
+print('-------------------')
+print(f'Winner: TBD')
+print('-------------------')
 
-# minvalue=df['Change'].min()
-# minindex=df['Change'].idxmin()
-# minmonth=df.loc[minindex,'Date']
-# #print(df)
-# with open(csvpath) as csvfile:
-
-#     # CSV reader used to declare delimiter and variable that holds contents into a variable I can reference
-#     csvreader = csv.reader(csvfile, delimiter=',')
-
-#     # Read the header row first (skip this step if there is now header)
-#     csv_header = next(csvreader)
-#     # print(f"CSV Header: {csv_header}")
-
-#     row_count=0
-#     Sum=0.0
-#     PandL=0.0
-
-#     # Tried to start this without pandas, so the count and P&L Sum is done without a dataframe
-#     for row in csvreader:
-
-#         PandL = float(row[1])
-#         Sum += PandL
-#         row_count+=1
-#        # averages=Sum/row_count
-
-
-     print('Financial Analysis')
-#     print('-------------------')
-#     print(f'Total Months: {row_count}')
 #     print(f'Total P&L: ${Sum}')
 #     print(f'Average Change: ${round(average,2)}')
 #     print(f'Greatest Increase in Profits: {maxmonth} ${maxvalue}')
