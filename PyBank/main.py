2d1fd43dce8d3822162a8956c065d0ddeabc6405
Dustin Elery <delery7@yahoo.com>
2021-01-07 14:48:19 -0500

    Cleaned up the PyBank main file comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,17 @@
-# print("adding a main file to PyBank")
 import os
 import csv
 import pandas as pd
 
+# Given my file creation on GitHub, I am getting a permission error I could not 
+# solve before due date running the statement directly below
 #csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 csvpath = r"C:\Users\deler\Desktop\python-challenge\PyBank\Resources\budget_data.csv"
-#print(csvpath)
 df=pd.read_csv(csvpath)
 
-difference=0.0
+# Adding a change column to my data frame using the P&L column
 df['Change']=df['Profit/Losses'] - df['Profit/Losses'].shift(1)
+
+# Doing the average, min, and max calculations using df's
 average=df['Change'].mean()
 maxvalue=df['Change'].max()
 maxindex=df['Change'].idxmax()
@@ -18,28 +20,31 @@ maxmonth=df.loc[maxindex,'Date']
 minvalue=df['Change'].min()
 minindex=df['Change'].idxmin()
 minmonth=df.loc[minindex,'Date']
-#print(df)
+
+# I tried to do these assignments without pandas but could not get creative enough from our coursework
+# After many google searches, everyone was indicating to use pandas so I switched my approach
+# I kept the non-pandas approach as it was working
+
 with open(csvpath) as csvfile:
 
     # CSV reader used to declare delimiter and variable that holds contents into a variable I can reference
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row first, so it could be skipped
     csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
 
+    # Declared the variables for the For loop below
     row_count=0
     Sum=0.0
     PandL=0.0
 
-    # Tried to start this without pandas, so the count and P&L Sum is done without a dataframe
     for row in csvreader:
 
         PandL = float(row[1])
         Sum += PandL
         row_count+=1
-       # averages=Sum/row_count
-
+  
+# run the print as README defined
 
     print('Financial Analysis')
     print('-------------------')
@@ -49,6 +54,7 @@ with open(csvpath) as csvfile:
     print(f'Greatest Increase in Profits: {maxmonth} ${maxvalue}')
     print(f'Greatest Decrease in Profits: {minmonth} ${minvalue}')
 
+# Pushing out the text file
     outputPath=r"C:\Users\deler\Desktop\python-challenge\PyBank\Analysis\Output.txt"
     with open(outputPath, 'w', newline='') as txtfile:
         txtfile.write('Financial Analysis\n')
