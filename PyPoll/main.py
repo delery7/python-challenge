2a3b637950b3038a3feadd8cfa5f6ead3169b03a
Dustin Elery <delery7@yahoo.com>
2021-01-07 14:38:24 -0500

    Cleaned up PyPoll main file with comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,37 +1,35 @@
-# print("adding a main file to PyBank")
 import os
 import csv
 import pandas as pd
 
-
+# Given my file creation on GitHub, I am getting a permission error I could not 
+# solve before due date running the statement directly below
 #csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
-csvpath = r"C:\Users\deler\Desktop\python-challenge\PyPoll\Resources\election_data.csv"
-#print(csvpath)
-df=pd.read_csv(csvpath)
-#print(df)
 
+# Used a direct path to work around the issue above
+csvpath = r"C:\Users\deler\Desktop\python-challenge\PyPoll\Resources\election_data.csv"
+df=pd.read_csv(csvpath)
+
+#Get a total count for the % calc
 vote_count=df['Voter ID'].nunique()
-# print(vote_count)
+
+#Calculate the count by candidate.  I used nunique as I am not familiar with the data set and wanted to account for possible duplicates
 cc=df.groupby(['Candidate']).nunique()
+
+#Make the %'s a whole number, as I found it easier to format around than find the syntax to hopefully format correctly
 ccpercent=(cc/vote_count)*100
+# Trimming the dataframes to the columns I need
 candidatesvotes=cc.iloc[:,0]
 candidatepercent=ccpercent.iloc[:,0]
-#provide column names to the DF's, then join on Candidate, then print each element independently
-# put print statements in a for loop as another idea
+
+# Joining the two tables to bring the data sets toghether, in hindsight, was not needed but it is how I got to the outcome mid development
 can_sum=pd.merge(left=candidatepercent, right=candidatesvotes, how='left', left_on='Candidate', right_on='Candidate')
+# Adding the index to put the candidate names back in the DF
 can_sum_idx = can_sum.reset_index(drop=False)
+# Sorted to have the code determine the winner
 can_sum_fnl = can_sum_idx.sort_values("Voter ID_y", ascending=False)
 
-
-# print(can_sum_idx)
-# print(can_sum_fnl)
-
-#print(cc)
-#print(ccpercent)
-#print(candidatesvotes)
-#print(candidatepercent)
-#print(can_sum)
-
+# Defining the variables so that I can customize the print statement to match the README instructions as best as possible
 can_nm1=can_sum_fnl.iloc[0,0]
 can_pct1=round(can_sum_fnl.iloc[0,1],2)
 can_tot1=can_sum_fnl.iloc[0,2]
